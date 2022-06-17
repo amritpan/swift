9b47021601355e68255d303bfc1d60e3e87ce2ec
Amritpan Kaur <you@example.com>
2022-06-16 20:12:30 -0700

    [ASTDumper] Remove list of Decls in Initial Constraints.

--- lib/AST/ASTDumper.cpp
+++ lib/AST/ASTDumper.cpp
@@ -2002,16 +2002,16 @@ public:
     PrintWithColorRAII(OS, IdentifierColor) << " name="
       << E->getDecls()[0]->getBaseName();
     PrintWithColorRAII(OS, ExprModifierColor)
-      << " number_of_decls=" << E->getDecls().size()
-      << " function_ref=" << getFunctionRefKindStr(E->getFunctionRefKind())
-      << " decls=[\n";
-    interleave(E->getDecls(),
-               [&](ValueDecl *D) {
-                 OS.indent(Indent + 2);
-                 D->dumpRef(PrintWithColorRAII(OS, DeclModifierColor).getOS());
-               },
-               [&] { PrintWithColorRAII(OS, DeclModifierColor) << ",\n"; });
-    PrintWithColorRAII(OS, ExprModifierColor) << "]";
+      << " number_of_decls=" << E->getDecls().size();
+//      << " function_ref=" << getFunctionRefKindStr(E->getFunctionRefKind())
+//      << " decls=[\n";
+//    interleave(E->getDecls(),
+//               [&](ValueDecl *D) {
+//                 OS.indent(Indent + 2);
+//                 D->dumpRef(PrintWithColorRAII(OS, DeclModifierColor).getOS());
+//               },
+//               [&] { PrintWithColorRAII(OS, DeclModifierColor) << ",\n"; });
+//    PrintWithColorRAII(OS, ExprModifierColor) << "]";
     PrintWithColorRAII(OS, ParenthesisColor) << ')';
   }
   void visitUnresolvedDeclRefExpr(UnresolvedDeclRefExpr *E) {
