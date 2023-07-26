95a3babf25d3518914f4267ace22b31a47f042d8
Amritpan Kaur <you@example.com>
2023-07-26 12:06:59 -0700

    Remove code

--- lib/Sema/CSSimplify.cpp
+++ lib/Sema/CSSimplify.cpp
@@ -12110,11 +12110,11 @@ ConstraintSystem::simplifyKeyPathConstraint(
 
     // If we have a malformed KeyPathExpr e.g. let _: KeyPath<A, C> = \A
     // let's record a AllowKeyPathMissingComponent fix.
-    if (keyPath->hasSingleInvalidComponent()) {
-      auto *fix = AllowKeyPathWithoutComponents::create(
-          *this, getConstraintLocator(locator));
-      return recordFix(fix) ? SolutionKind::Error : SolutionKind::Solved;
-    }
+//    if (keyPath->hasSingleInvalidComponent()) {
+//      auto *fix = AllowKeyPathWithoutComponents::create(
+//          *this, getConstraintLocator(locator));
+//      return recordFix(fix) ? SolutionKind::Error : SolutionKind::Solved;
+//    }
 
     // If the root type has been bound to a hole, we cannot infer it.
     if (getFixedTypeRecursive(rootTy, /*wantRValue*/ true)->isPlaceholder())
