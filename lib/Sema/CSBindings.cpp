4ea64d4f44e2363ee033b54c0803529592f8ea23
Amritpan Kaur <you@example.com>
2023-07-31 15:57:21 -0700

    Separate 1

--- lib/Sema/CSBindings.cpp
+++ lib/Sema/CSBindings.cpp
@@ -1250,27 +1250,32 @@ PotentialBindings::inferFromRelational(Constraint *constraint) {
     return llvm::None;
 
   if (TypeVar->getImpl().isKeyPathType()) {
-    auto *BGT = type->lookThroughAllOptionalTypes()->getAs<BoundGenericType>();
-    if (!BGT || !isKnownKeyPathType(BGT))
+    auto objectTy = type->lookThroughAllOptionalTypes();
+    if (!isKnownKeyPathType(objectTy))
       return llvm::None;
+    
+    auto &ctx = CS.getASTContext();
+    auto *keyPathTypeLoc = TypeVar->getImpl().getLocator();
+    auto *keyPath = castToExpr<KeyPathExpr>(keyPathTypeLoc->getAnchor());
+    if (objectTy->isAnyKeyPath()) {
 
-    // `PartialKeyPath<T>` represents a type-erased version of `KeyPath<T, V>`.
-    //
-    // In situations where partial key path cannot be used directly i.e.
-    // passing an argument to a parameter represented by a partial key path,
-    // let's attempt a `KeyPath` binding which would then be converted to a
-    // partial key path since there is a subtype relationship between them.
-    if (BGT->isPartialKeyPath() && kind == AllowedBindingKind::Subtypes) {
-      auto &ctx = CS.getASTContext();
-      auto *keyPathLoc = TypeVar->getImpl().getLocator();
-
-      auto rootTy = BGT->getGenericArgs()[0];
+      auto root = CS.getKeyPathRootType(keyPath);
+      auto value = CS.getKeyPathValueType(keyPath);
+      
+      type = BoundGenericType::get(ctx.getKeyPathDecl(), Type(),
+                                   {root, value});
+    } else if (objectTy->isPartialKeyPath() && kind == AllowedBindingKind::Subtypes) {
+      // `PartialKeyPath<T>` represents a type-erased version of `KeyPath<T, V>`.
+      //
+      // In situations where partial key path cannot be used directly i.e.
+      // passing an argument to a parameter represented by a partial key path,
+      // let's attempt a `KeyPath` binding which would then be converted to a
+      // partial key path since there is a subtype relationship between them.
+      auto rootTy = objectTy->castTo<BoundGenericType>()->getGenericArgs()[0];
       // Since partial key path is an erased version of `KeyPath`, the value
       // type would never be used, which means that binding can use
       // type variable generated for a result of key path expression.
-      auto valueTy =
-          keyPathLoc->castLastElementTo<LocatorPathElt::KeyPathType>()
-              .getValueType();
+      auto valueTy = CS.getKeyPathValueType(keyPath);
 
       type = BoundGenericType::get(ctx.getKeyPathDecl(), Type(),
                                    {rootTy, valueTy});
