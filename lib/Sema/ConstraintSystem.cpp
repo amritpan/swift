99aa84332506a3d10a4b631202c9afab07adf38b
Amritpan Kaur <amritpankaur@gmail.com>
2024-09-05 10:49:51 -0700

    [ConstraintSystem] Evaluate constraints for keypath methods.

--- lib/Sema/ConstraintSystem.cpp
+++ lib/Sema/ConstraintSystem.cpp
@@ -724,6 +724,8 @@ ConstraintLocator *ConstraintSystem::getCalleeLocator(
     case ComponentKind::TupleElement:
       llvm_unreachable("Not implemented by CSGen");
       break;
+    case ComponentKind::UnresolvedApply:
+    case ComponentKind::Apply:
     case ComponentKind::Invalid:
     case ComponentKind::OptionalForce:
     case ComponentKind::OptionalChain:
@@ -7624,6 +7626,8 @@ ConstraintSystem::inferKeyPathLiteralCapability(KeyPathExpr *keyPath) {
       return fail();
     }
 
+    case KeyPathExpr::Component::Kind::Apply:
+    case KeyPathExpr::Component::Kind::UnresolvedApply:
     case KeyPathExpr::Component::Kind::UnresolvedSubscript:
     case KeyPathExpr::Component::Kind::Subscript: {
       if (Context.LangOpts.hasFeature(Feature::InferSendableFromCaptures)) {
