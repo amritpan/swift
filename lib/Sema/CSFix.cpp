d00785ad305ede5b737e392fca58632e477989bd
Amritpan Kaur <amritpankaur@gmail.com>
2024-09-05 18:24:47 -0700

    [CSDiagnostics] Remove checks preventing method keypaths.

--- lib/Sema/CSFix.cpp
+++ lib/Sema/CSFix.cpp
@@ -1245,7 +1245,6 @@ bool AllowInvalidRefInKeyPath::diagnose(const Solution &solution,
     return failure.diagnose(asNote);
   }
 
-  case RefKind::Method:
   case RefKind::Initializer: {
     InvalidMethodRefInKeyPath failure(solution, Member, getLocator());
     return failure.diagnose(asNote);
@@ -1286,12 +1285,6 @@ AllowInvalidRefInKeyPath::forRef(ConstraintSystem &cs, Type baseType,
           Feature::KeyPathWithStaticMembers) &&
       member->isStatic())
     return AllowInvalidRefInKeyPath::create(cs, baseType, RefKind::StaticMember,
-                                            member, locator);
-
-  // Referencing (instance or static) methods in key path is
-  // not currently allowed.
-  if (isa<FuncDecl>(member))
-    return AllowInvalidRefInKeyPath::create(cs, baseType, RefKind::Method,
                                             member, locator);
 
   // Referencing enum cases in key path is not currently allowed.
