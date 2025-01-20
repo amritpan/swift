c62ffe14675c5f78f629c119feee3b2dd2ae6b3c
Amritpan Kaur <amritpankaur@gmail.com>
2025-01-20 10:44:34 -0800

    Guard feature behind experimental flag.

--- lib/Sema/CSDiagnostics.cpp
+++ lib/Sema/CSDiagnostics.cpp
@@ -6417,6 +6417,12 @@ bool InvalidMemberWithMutatingGetterInKeyPath::diagnoseAsError() {
   return true;
 }
 
+bool InvalidMethodRefInKeyPath::diagnoseAsError() {
+  emitDiagnostic(diag::expr_keypath_not_property, getMember(),
+                 isForKeyPathDynamicMemberLookup());
+  return true;
+}
+
 bool InvalidMutatingMethodRefInKeyPath::diagnoseAsError() {
   emitDiagnostic(diag::expr_keypath_mutating_method, getMember(),
                  isForKeyPathDynamicMemberLookup());
